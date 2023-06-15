8d0e7b83c31a7c5adeef37556d4931141e859ce8
Nevin <nevinthomas48@gmail.com>
2023-06-15 21:05:31 +0530

    Backed

--- front/src/Components/Authentication/AdminDas.jsx
+++ front/src/Components/Authentication/AdminDas.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Alert from '@material-ui/lab/Alert';
+import axios from 'axios';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+  userContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: theme.spacing(1),
+  },
+  userInfo: {
+    marginRight: theme.spacing(2),
+  },
+}));
 
 const AdminDas = () => {
-  return (
-    <div>AdminDas</div>
-  )
-}
+  const classes = useStyles();
+  const [users, setUsers] = useState([]);
+  const [showAlert, setShowAlert] = useState(false);
 
-export default AdminDas
+  useEffect(() => {
+    // Fetch users from the database
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get('/api/users'); // Replace with your backend API endpoint to fetch users
+      setUsers(response.data);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  };
+
+  const deleteUser = async (userId) => {
+    try {
+      await axios.delete(`/api/users/${userId}`); // Replace with your backend API endpoint to delete a user
+      setShowAlert(true);
+      // Refresh the user list
+      fetchUsers();
+    } catch (error) {
+      console.error('Error deleting user:', error);
+    }
+  };
+
+  return (
+    <div className={classes.root}>
+      <Typography variant="h5">Admin Dashboard</Typography>
+      {showAlert && (
+        <Alert severity="success" onClose={() => setShowAlert(false)}>
+          User deleted successfully.
+        </Alert>
+      )}
+      {users.map((user) => (
+        <div key={user._id} className={classes.userContainer}>
+          <div className={classes.userInfo}>
+            <Typography>{user.name}</Typography>
+            <Typography variant="body2">{user.email}</Typography>
+          </div>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => deleteUser(user._id)}
+          >
+            Delete
+          </Button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AdminDas;
--- front/src/Components/Authentication/SignInForm.jsx
+++ front/src/Components/Authentication/SignInForm.jsx
@@ -2,24 +2,25 @@ import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import FormControl from '@material-ui/core/FormControl';
-// import FormLabel from '@material-ui/core/FormLabel';
-// import RadioGroup from '@material-ui/core/RadioGroup';
-// import Radio from '@material-ui/core/Radio';
-// import Select from '@material-ui/core/Select';
-// import MenuItem from '@material-ui/core/MenuItem';
-// import Slider from '@material-ui/core/Slider';
 import Button from '@material-ui/core/Button';
 import { LockOutlined } from '@material-ui/icons';
-import './SignInStyles.css'
+import axios from 'axios';
+import './SignInStyles.css';
 
 const SignInForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log('email:', email);
-    console.log('password:', password);
+
+    try {
+      const response = await axios.post('createProfile', { email, password });
+      // Handle successful login here
+      console.log(response.data); // Assuming the response contains the created profile data
+    } catch (error) {
+      console.error('Error signing in:', error);
+    }
   };
 
   const handleEmailChange = (event) => {
@@ -31,35 +32,35 @@ const SignInForm = () => {
   };
 
   return (
-    <div className='shape'>
-    <form onSubmit={handleSubmit} className='signin-form'>
-      <Grid container direction="column" alignItems="center">
-        <Grid item>
-          <LockOutlined />
+    <div className="shape">
+      <form onSubmit={handleSubmit} className="signin-form">
+        <Grid container direction="column" alignItems="center">
+          <Grid item>
+            <LockOutlined />
+          </Grid>
+          <Grid item>
+            <FormControl>
+              <TextField
+                id="email"
+                label="Email"
+                type="email"
+                value={email}
+                onChange={handleEmailChange}
+              />
+              <TextField
+                id="password"
+                label="Password"
+                type="password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+              <Button type="submit" variant="contained" color="primary">
+                Sign In
+              </Button>
+            </FormControl>
+          </Grid>
         </Grid>
-        <Grid item>
-          <FormControl>
-            <TextField
-              id="email"
-              label="Email"
-              type="email"
-              value={email}
-              onChange={handleEmailChange}
-            />
-            <TextField
-              id="password"
-              label="Password"
-              type="password"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-            <Button type="submit" variant="contained" color="primary">
-              Sign In
-            </Button>
-          </FormControl>
-        </Grid>
-      </Grid>
-    </form>
+      </form>
     </div>
   );
 };
