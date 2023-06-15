da364bd0c4e2fdf6c6b644019e81a66abc2a3397
Prithviraj V Nair <94970860+PrithvirajVNair@users.noreply.github.com>
2023-06-15 18:30:28 +0530

    Update ExpenseModel.js

--- backend/models/ExpenseModel.js
+++ backend/models/ExpenseModel.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://jebinshaju:jebinshaju@cluster0.s0sj5ym.mongodb.net/?retryWrites=true&w=majority")
-    .then(() => {
-        console.log("db connected");
-    })
-    .catch(err => console.log(err))
+
 
 const ExpenseSchema = new mongoose.Schema({
     title: {
