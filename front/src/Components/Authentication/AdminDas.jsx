483c32090a7dab6577645e1cded237e814f6729c
Nevin <nevinthomas48@gmail.com>
2023-06-16 19:39:41 +0530

    Admin Dashboard

--- front/src/Components/Authentication/AdminDas.jsx
+++ front/src/Components/Authentication/AdminDas.jsx
@@ -8,6 +8,18 @@ import axios from 'axios';
 const useStyles = makeStyles((theme) => ({
   root: {
     padding: theme.spacing(2),
+    textAlign: 'center',
+    overflow: 'auto',
+    maxHeight: '1000px',
+  },
+  tableContainer: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  table: {
+    width: '80%',
+    marginTop: theme.spacing(2),
   },
   userContainer: {
     display: 'flex',
@@ -15,37 +27,44 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: theme.spacing(1),
   },
   userInfo: {
+    flex: 1,
     marginRight: theme.spacing(2),
   },
 }));
 
 const AdminDas = () => {
   const classes = useStyles();
-  const [users, setUsers] = useState([]);
+  const [profiles, setProfiles] = useState([]);
   const [showAlert, setShowAlert] = useState(false);
 
   useEffect(() => {
-    // Fetch users from the database
-    fetchUsers();
+    // Fetch profiles from the API
+    fetchProfiles();
+    
   }, []);
+  window.location.reload();
 
-  const fetchUsers = async () => {
+  const fetchProfiles = async () => {
     try {
-      const response = await axios.get('/api/users'); // Replace with your backend API endpoint to fetch users
-      setUsers(response.data);
+      
+      const response = await axios.get('http://localhost:8080/api/v1/getProfiles'); // Replace with your backend API endpoint to fetch profiles
+      const profilesArray = Object.values(response.data); // Convert the profiles object into an array
+      setProfiles(profilesArray);
     } catch (error) {
-      console.error('Error fetching users:', error);
+      console.error('Error fetching profiles:', error);
     }
   };
 
-  const deleteUser = async (userId) => {
+  const deleteProfile = async (profileId) => {
     try {
-      await axios.delete(`/api/users/${userId}`); // Replace with your backend API endpoint to delete a user
+      await axios.delete(`http://localhost:8080/api/v1/deleteProfile/${profileId}`);
+      console.log(profileId)
+       // Replace with your backend API endpoint to delete a profile
       setShowAlert(true);
-      // Refresh the user list
-      fetchUsers();
+      // Refresh the profile list
+      fetchProfiles();
     } catch (error) {
-      console.error('Error deleting user:', error);
+      console.error('Error deleting profile:', error);
     }
   };
 
@@ -54,24 +73,41 @@ const AdminDas = () => {
       <Typography variant="h5">Admin Dashboard</Typography>
       {showAlert && (
         <Alert severity="success" onClose={() => setShowAlert(false)}>
-          User deleted successfully.
+          Profile deleted successfully.
         </Alert>
       )}
-      {users.map((user) => (
-        <div key={user._id} className={classes.userContainer}>
-          <div className={classes.userInfo}>
-            <Typography>{user.name}</Typography>
-            <Typography variant="body2">{user.email}</Typography>
-          </div>
-          <Button
-            variant="contained"
-            color="secondary"
-            onClick={() => deleteUser(user._id)}
-          >
-            Delete
-          </Button>
+      {profiles.length === 0 ? (
+        <Typography>No profiles found.</Typography>
+      ) : (
+        <div className={classes.tableContainer}>
+          <table className={classes.table}>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {profiles[1].map((profile) => (
+                <tr key={profile._id}>
+                  <td>{profile.name}</td>
+                  <td>{profile.email}</td>
+                  <td>
+                    <Button
+                      variant="contained"
+                      color="secondary"
+                      onClick={() => deleteProfile(profile._id)}
+                    >
+                      Delete
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-      ))}
+      )}
     </div>
   );
 };
