4f8b145683e458843f040724ea0147a0917423f1
Nevin <nevinthomas48@gmail.com>
2023-06-19 19:30:19 +0530

    Fixed expense form glitches

--- front/src/Components/Expenses/ExpenseForm.js
+++ front/src/Components/Expenses/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 
-const API_URL = 'http://localhost:8080/api/v1/';
+const API_URL = 'https://spendsence.onrender.com/api/v1/';
 
 const ProfileContainer = styled.div`
   display: flex;
