104d0a6e6ee74850c92fae1ee50c829052e6d0fe
Jebin Shaju <jebinshaju4@gmail.com>
2023-06-19 23:49:59 +0530

    ADDED BLOCKING

--- front/src/Components/Authentication/AdminDas.jsx
+++ front/src/Components/Authentication/AdminDas.jsx
@@ -1,36 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import Alert from '@material-ui/lab/Alert';
-import axios from 'axios';
-import { useHistory } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Alert from "@material-ui/lab/Alert";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
+import BlockIcon from "@mui/icons-material/Block";
+import Paper from "@material-ui/core/Paper";
+import { flexbox } from "@mui/system";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     padding: theme.spacing(2),
-    textAlign: 'center',
-    overflow: 'auto',
-    maxHeight: '500px',
+    textAlign: "center",
+    overflow: "auto",
+    maxHeight: "500px",
   },
   tableContainer: {
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'center',
+    display: "flex",
+    justifyContent: "center",
   },
   table: {
-    width: '80%',
+    width: "80%",
     marginTop: theme.spacing(2),
+    borderRadius: theme.spacing(2),
+    boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.1)",
+    textAlign: "center",
+    ...flexbox.center, // Apply flexbox utility class to center the table
   },
   userContainer: {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
     marginBottom: theme.spacing(1),
   },
   signoutButton: {
-    position: 'absolute',
+    position: "absolute",
     right: theme.spacing(2),
-    bottom: theme.spacing(2),
+    bottom: theme.spacing(1),
   },
 }));
 
@@ -39,6 +47,8 @@ const AdminDas = () => {
   const history = useHistory();
   const [profiles, setProfiles] = useState([]);
   const [showAlert, setShowAlert] = useState(false);
+  const [blockedProfiles, setBlockedProfiles] = useState([]);
+
 
   useEffect(() => {
     fetchProfiles();
@@ -50,74 +60,132 @@ const AdminDas = () => {
 
   const fetchProfiles = async () => {
     try {
-      const response = await axios.get('https://spendsence.onrender.com/api/v1/getProfiles');
+      const response = await axios.get(
+        "https://spendsence.onrender.com/api/v1/getProfiles"
+      );
       const profilesArray = Object.values(response.data);
       setProfiles(profilesArray);
     } catch (error) {
-      console.error('Error fetching profiles:', error);
+      console.error("Error fetching profiles:", error);
     }
   };
 
   const deleteProfile = async (profileId) => {
     try {
-      await axios.delete(`https://spendsence.onrender.com/api/v1/deleteProfile/${profileId}`);
+      await axios.delete(
+        `https://spendsence.onrender.com/api/v1/deleteProfile/${profileId}`
+      );
       setShowAlert(true);
       fetchProfiles();
     } catch (error) {
-      console.error('Error deleting profile:', error);
+      console.error("Error deleting profile:", error);
     }
   };
 
+  const blockProfile = async (profileId) => {
+    try {
+      const updatedProfiles = [...blockedProfiles];
+      const index = updatedProfiles.indexOf(profileId);
+  
+      if (index === -1) {
+        // Profile is not blocked, so block it
+        updatedProfiles.push(profileId);
+        setShowAlert('blocked');
+      } else {
+        // Profile is blocked, so unblock it
+        updatedProfiles.splice(index, 1);
+        setShowAlert('unblocked');
+      }
+  
+      setBlockedProfiles(updatedProfiles);
+      fetchProfiles();
+    } catch (error) {
+      console.error('Error blocking/unblocking profile:', error);
+    }
+    try {
+      // Make the necessary API call to block the profile
+      await axios.post(`https://spendsence.onrender.com/api/v1/blockProfile/${profileId}`);
+      setShowAlert(true);
+      fetchProfiles();
+    } catch (error) {
+      console.error('Error blocking profile:', error);
+    }
+  };
+  
+
   const signOut = () => {
-    history.push('/');
-    window.location.reload()
+    history.push("/");
+    window.location.reload();
   };
 
   return (
     <div className={classes.root}>
-      <Typography variant="h5" style={{fontFamily:'sans-serif',fontSize:'2rem'}}>Admin Dashboard</Typography>
+      <Typography
+        variant="h5"
+        style={{ fontFamily: "sans-serif", fontSize: "2rem" }}
+      >
+        Admin Dashboard
+      </Typography>
       <br />
       <hr />
       {showAlert && (
         <Alert severity="success" onClose={() => setShowAlert(false)}>
-          Profile deleted successfully.
+          Profile action performed successfully.
         </Alert>
       )}
-      
+
       {profiles.length === 0 ? (
         <Typography>No profiles found.</Typography>
       ) : (
         <div className={classes.tableContainer}>
-          <table className={classes.table}>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {profiles[1].map((profile) => (
-                <tr key={profile._id}>
-                  <td>{profile.name}</td>
-                  <td>{profile.email}</td>
-                  <td>
-                    <Button
-                      variant="contained"
-                      color="secondary"
-                      onClick={() => deleteProfile(profile._id)}
-                    >
-                      Delete
-                    </Button>
-                  </td>
+          <Paper elevation={3} className={classes.table}>
+            <table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Actions</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-      
+              </thead>
+              <tbody>
+                {profiles[1].map((profile) => (
+                  <tr
+                    key={profile._id}
+                    style={{
+                      color: blockedProfiles.includes(profile._id)
+                        ? "red"
+                        : "inherit",
+                    }}
+                  >
+                    <td>{profile.name}</td>
+                    <td>{profile.email}</td>
+                    <td>
+                      <IconButton
+                        color="secondary"
+                        onClick={() => deleteProfile(profile._id)}
+                      >
+                        <DeleteIcon />
+                      </IconButton>
+                      <IconButton
+                        color="secondary"
+                        onClick={() => blockProfile(profile._id)}
+                      >
+                        <BlockIcon />
+                      </IconButton>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </Paper>
         </div>
       )}
-      <Button variant="contained" color="primary" onClick={signOut} className={classes.signoutButton}> 
+      <Button
+        variant="contained"
+        color="primary"
+        className={classes.signoutButton}
+        onClick={signOut}
+      >
         Sign Out
       </Button>
     </div>
